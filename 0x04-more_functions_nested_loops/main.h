bb6c1fedb0453e16d29cb0ef49a3a7ca4e5e90a0
KarimiMuturi1 <karimimuturi0@gmail.com>
2023-02-23 02:40:05 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * file main.h
+ * Author: Karimi
+ * Desc: Header contains prototype for all functions used in the project 0x04-more functions
+ */
+int _putchar(char c);
+
+#endif
