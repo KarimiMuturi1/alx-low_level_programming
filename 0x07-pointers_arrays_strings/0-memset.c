3e9469f61c852a9c06f98a3d62cfebc04de28368
KarimiMuturi1 <karimimuturi0@gmail.com>
2023-03-06 10:41:34 -0800

    correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-/**
+/*
  * _memset - a function that fills
  *         memory with a constant byte
  *
@@ -17,7 +17,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
+	/*
 	 * declare an unsigned int
 	 * because we are storing a
 	 * value that will always be
